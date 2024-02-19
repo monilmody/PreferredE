b3efe28f8bb3d011b2ec8a1fe5b2999493e3f369
swiftrails <swiftrails2024@gmail.com>
2024-02-19 16:22:21 -0500

    2/19/2024 - 3:16 : fixing the syntax issue in sireAnalysis -13

--- functions.php
+++ functions.php
@@ -781,7 +781,7 @@ function fetchSireData_tb($sire,$year,$elig,$gait,$sort1,$sort2,$sort3,$sort4,$s
         Rating
         FROM tsales a
         JOIN tdamsire b ON a.damsire_Id=b.damsire_ID
-        WHERE TYPE= "Y" AND PRICE>0) AS a;';
+        WHERE TYPE= "Y" AND PRICE>0)';
     
     $join = ') a LEFT JOIN
     (SELECT Price AS Rankprice ,(@curRank := @curRank + 1) AS Rank from (
