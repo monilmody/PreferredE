1394268928556a3d657b05f3f12244b32fde90d9
swiftrails <swiftrails2024@gmail.com>
2024-02-20 14:57:17 -0500

    2/19/2024 - 3:16 : fixing the syntax issue in sireAnalysis -30

--- sire_analysis_tb.php
+++ sire_analysis_tb.php
@@ -205,11 +205,9 @@ if ($year_param != "" or $sire_param != "" or $elig_param != "") {
                   #echo $row[Price];
                   foreach($row1 as $elements) {
                       $elementCount =$elementCount+1;
-                      if($elementCount == 17){
-                          $elements = "$".number_format($elements);
-                      }
-                      if ($elements == "0000-00-00") {
-                          $elements="";
+                      // Check if $elements is numeric before formatting
+                      if (($elementCount == 17) && is_numeric($elements)) {
+                        $elements = "$" . number_format((float)$elements);
                       }
                       // Check and format date fields
                       if ($elementCount == 9 || $elementCount == 15) {
