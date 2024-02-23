ae10d1d5c7b8b887d0f25b1705d8b2bedb858907
swiftrails <swiftrails2024@gmail.com>
2024-02-23 18:10:40 -0500

    2/19/2024 - 3:16 : fixing the query for auction report

--- functions.php
+++ functions.php
@@ -2506,8 +2506,8 @@ function fetchSalesSummary_tb($year,$type,$salecode)
                      AND left(Salecode,4)= IF("'.$salecode.'"  = "", left(Salecode,4), "'.$salecode.'") ';
     
     $sql = 'SELECT a.Salecode,a.Horse As PACER, a.Max AS PMax,MAX(b.Horse) As Trotter,b.Max As TMax FROM
-    (SELECT Salecode, a.Horse, MAX(Price) as Max FROM tsales WHERE GAIT ="P" '.$searchParam.'
-        GROUP BY Salecode,horse ORDER BY salecode,MAX(Price) Desc) a
+    (SELECT Salecode, Horse, MAX(Price) as Max FROM tsales WHERE GAIT ="P" '.$searchParam.'
+        GROUP BY Salecode,Horse ORDER BY salecode,MAX(Price) Desc) a
     LEFT JOIN
     (SELECT Salecode, Horse, MAX(Price) AS Max FROM tsales WHERE GAIT ="T" '.$searchParam.'
         GROUP BY Salecode,Horse ORDER BY Salecode,MAX(Price) Desc) b on a.Salecode=b.Salecode Group by salecode';
