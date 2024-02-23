7a58e42f67c9252423ae43e529e1700b899bf731
swiftrails <swiftrails2024@gmail.com>
2024-02-23 17:29:05 -0500

    2/19/2024 - 3:16 : fixing the query for auction report

--- functions.php
+++ functions.php
@@ -2505,12 +2505,12 @@ function fetchSalesSummary_tb($year,$type,$salecode)
                      AND Type= IF("'.$type.'"  = "", Type, "'.$type.'")
                      AND left(Salecode,4)= IF("'.$salecode.'"  = "", left(Salecode,4), "'.$salecode.'") ';
     
-    $sql = 'SELECT a.Salecode,a.Horse As PACER, a.Max AS PMax,b.Horse As Trotter,b.Max As TMax FROM
+    $sql = 'SELECT a.Salecode,a.Horse As PACER, a.Max AS PMax, MAX(b.Horse) As Trotter,b.Max As TMax FROM
     (SELECT Salecode, Horse, Price as Max FROM tsales WHERE GAIT ="P" '.$searchParam.'
         GROUP BY Salecode,horse ORDER BY salecode,Price Desc) a
     LEFT JOIN
     (SELECT Salecode, Horse, Price AS Max FROM tsales WHERE GAIT ="T" '.$searchParam.'
-        GROUP BY Salecode,Horse ORDER BY Salecode,Price Desc) b on a.Salecode=b.Salecode Group by salecode';
+        GROUP BY Salecode,Horse ORDER BY Salecode,Price Desc) b on a.Salecode=b.Salecode Group by a.salecode';
     
     //     if ($year != "") {
     //         $sql = "SELECT a.Salecode,a.Horse As PACER, a.Max AS PMax,b.Horse As Trotter,b.Max As TMax FROM
