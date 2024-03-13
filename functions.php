6b81537b133a707854cda59243256dd08073259a
swiftrails <swiftrails2024@gmail.com>
2024-03-13 14:59:07 -0400

    fixing the allignment

--- functions.php
+++ functions.php
@@ -646,13 +646,6 @@ function fetchSireData($sire,$year,$elig,$gait,$sort1,$sort2,$sort3,$sort4,$sort
     $searchElig = ' AND Elig= "'.$elig.'" ';
     $searchGait = ' AND Gait= "'.$gait.'" ';
     
-    $orderby1 = ' ORDER BY '.$sort1;
-    $orderby2 = ', '.$sort2;
-    $orderby3 = ', '.$sort3;
-    $orderby4 = ', '.$sort4;
-    $orderby5 = ', '.$sort5;
-    
-   
     $join11 = ' group by Price ORDER BY Price desc) as a,(SELECT @curRank := 0) r) b
     on a.price=b.Rankprice '; //in order to do ranking becauserank function doesn't work on server.
     $join21 = ' group by price,sex ORDER BY price desc) as a,(SELECT @curRank1 := 0) r) c
@@ -712,6 +705,11 @@ function fetchSireData($sire,$year,$elig,$gait,$sort1,$sort2,$sort3,$sort4,$sort
         $sql = $sql.$searchElig.$join.$searchElig.$join11.$join1.$searchElig.$join21.$join2.$searchElig.$join31;
     }
     
+    $orderby1 = ' ORDER BY '.$sort1;
+    $orderby2 = ', '.$sort2;
+    $orderby3 = ', '.$sort3;
+    $orderby4 = ', '.$sort4;
+    $orderby5 = ', '.$sort5;
     
     if ($sort1 !="" && $sort2 !="" && $sort3 !="" && $sort4 !="" && $sort5 !="") {
         $sql = $sql.$orderby1.$orderby2.$orderby3.$orderby4.$orderby5;
@@ -1066,12 +1064,6 @@ function fetchSireData_tb($sire,$year,$elig,$gait,$sort1,$sort2,$sort3,$sort4,$s
     $searchElig = ' AND Elig= "'.$elig.'" ';
     $searchGait = ' AND Gait= "'.$gait.'" ';
     
-    $orderby1 = ' ORDER BY '.$sort1;
-    $orderby2 = ', '.$sort2;
-    $orderby3 = ', '.$sort3;
-    $orderby4 = ', '.$sort4;
-    $orderby5 = ', '.$sort5;
-    
    
     $join11 = ' group by Price ORDER BY Price desc) as a,(SELECT @curRank := 0) r) b
     on a.price=b.Rankprice '; //in order to do ranking becauserank function doesn't work on server.
@@ -1132,6 +1124,11 @@ function fetchSireData_tb($sire,$year,$elig,$gait,$sort1,$sort2,$sort3,$sort4,$s
         $sql = $sql.$searchElig.$join.$searchElig.$join11.$join1.$searchElig.$join21.$join2.$searchElig.$join31;
     }
     
+    $orderby1 = ' ORDER BY '.$sort1;
+    $orderby2 = ', '.$sort2;
+    $orderby3 = ', '.$sort3;
+    $orderby4 = ', '.$sort4;
+    $orderby5 = ', '.$sort5;
     
     if ($sort1 !="" && $sort2 !="" && $sort3 !="" && $sort4 !="" && $sort5 !="") {
         $sql = $sql.$orderby1.$orderby2.$orderby3.$orderby4.$orderby5;
