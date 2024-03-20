bd7a6ec1ffdcc8b16f780332194441e8320b154c
swiftrails <swiftrails2024@gmail.com>
2024-03-20 13:54:15 -0400

    onchange function

--- broodmares_report_tb.php
+++ broodmares_report_tb.php
@@ -50,14 +50,14 @@ $sortList = array("Hip","Horse","Type", "Gait", "Price Desc", "Salecode", "Day",
 <!-- </b> -->
 <br>
 <br>
-<select class="custom-select1" id="year">
+<select class="custom-select1" id="year"> onchange="location = this.value;"
 	<option value="">Sale Year</option>
 	<option value="">All Years</option>
 	<?php foreach($yearList as $row) {
 	    echo '<option>'.$row['Year'].'</option>';
     } ?>
 </select>
- <select class="custom-select1" id="salecode">
+ <select class="custom-select1" id="salecode"> onchange="location = this.value;"
 	<option value="">Sale Code Filter</option>
 	<option value="">All Salecode</option>
 	<?php foreach($resultList as $row) {
