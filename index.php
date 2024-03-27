b6758317bda4d75f58820f4687d10b5c9cc95986
swiftrails <swiftrails2024@gmail.com>
2024-03-27 15:37:57 -0400

    pushing the fivicon change

--- index.php
+++ index.php
@@ -25,7 +25,8 @@ require_once("config.php");
   gtag('config', 'UA-18763673-4');
 </script>
 
-    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    <meta name="description" content="">    <meta name="author" content="">    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">    <title>Preferred Equine</title>    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">    <link rel="stylesheet" href="assets/css/style.css"></head>
+    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    <meta name="description" content="">    <meta name="author" content="">    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">    <title>Preferred Equine</title>    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">    <link rel="stylesheet" href="assets/css/style.css">      <link rel="icon" href="assets\images\favicon.ico" type="image/x-icon">
+</head>
     
     <body>
     
