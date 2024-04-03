f0f4fde9925456b2964700cd111a80ff511c3c2d
Monil Mody <40249034+monilmody@users.noreply.github.com>
2024-04-03 12:23:43 -0400

    Update manage_data.php

--- manage_data.php
+++ manage_data.php
@@ -10,7 +10,8 @@ include("./header.php");
   <link rel="stylesheet" href="assets/css/table.css">
   
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+  <script type="text/javascript" charset="utf8"
+    src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
   <script src="assets/js/script.js"></script>
 </head>
 <?php
@@ -29,20 +30,10 @@ if ($breed_param == "T") {
 }
 
 if (!empty($_POST)) {
-  $saledate = trim($_POST["saledate"]);
-  // Construct the delete query based on the sale date
-  $sql_delete = "DELETE FROM sales WHERE Saledate = '$saledate'";
-  if ($breed_param == "T") {
-    $sql_delete = "DELETE FROM tsales WHERE Saledate = '$saledate'";
-  }
-
-  // Execute the delete query
-  $result = mysqli_query($link, $sql_delete);
-  if ($result) {
-    echo "Record deleted successfully";
-  } else {
-    echo "Error deleting record: " . mysqli_error($link);
-  }
+  $salecode = trim($_POST["salecode"]);
+  //$password = trim($_POST["password"]);
+  //echo("alert('aaaa')");
+  echo $salecode;
 }
 ?>
 
@@ -50,25 +41,55 @@ if (!empty($_POST)) {
 
 <div style="margin:5px 30px 30px 30px;">
   <h1 style="text-align:center;color:#D98880;">Manage File Upload Data</h1>
+
+
+
   <select style="background-color:#229954;" class="custom-select1" id="breed">
     <option value="">Breed Filter</option>
     <option value="S">S : Standardbred</option>
     <option value="T">T : Thoroughbred</option>
+
   </select>
-  <input class="custom-select1" type="submit" onclick="getValues()" name="SUBMITBUTTON" value="Submit" style="font-size:20px; " />
+
+  <input class="custom-select1" type="submit" onclick="getValues()" name="SUBMITBUTTON" value="Submit"
+    style="font-size:20px; " />
+
+
   <hr>
   <div style="max-height: calc(96.2vh - 96.2px);overflow:auto;">
     <div class="table" style="width: device-width;">
       <div class="row header blue" style="line-height: 25px;font-size: 12px;position: sticky;top: 0;">
         <table id="salesTable">
-          <div class="cell" style="width: device-width;">No.</div>
-          <div class="cell" style="width: device-width;">Salecode<button onclick="sortTable('Salecode')"><img src="assets\images\sort.png" alt="Sort Salecode"></button></div>
-          <div class="cell" style="width: device-width;">Saledate<button onclick="sortTable('Saledate')"><img src="assets\images\sort.png" alt="Sort Saledate"></button></div>
-          <div class="cell" style="width:device-width;">Upload-date<button onclick="sortTable('upload_date')"><img src="assets\images\sort.png" alt="Sort Uploadtime"></button></div>
-          <div class="cell" style="width: device-width;">Salecount</div>
-          <div class="cell" style="width: device-width;">delete</div>
+          <div class="cell" style="width: device-width;">
+            No.
+          </div>
+          <div class="cell" style="width: device-width;">
+            Salecode
+            <button onclick="sortTable('Salecode')">
+              <img src="assets\images\sort.png" alt="Sort Salecode">
+            </button>
+          </div>
+          <div class="cell" style="width: device-width;">
+            Saledate
+            <button onclick="sortTable('Saledate')">
+              <img src="assets\images\sort.png" alt="Sort Saledate">
+            </button>
+          </div>
+	  <div class="cell" style="width:device-width;">
+            Upload-date
+            <button onclick="sortTable('upload_date')">
+              <img src="assets\images\sort.png" alt="Sort Uploadtime">
+            </button>
+          </div>
+          <div class="cell" style="width: device-width;">
+            Salecount
+          </div>
+          <div class="cell" style="width: device-width;">
+            delete
+          </div>
         </table>
       </div>
+
       <?php
       setlocale(LC_MONETARY, "en_US");
       $number = 0;
@@ -90,20 +111,26 @@ if (!empty($_POST)) {
           }
           echo "<div class='cell'>" . $elements . "</div>";
         }
+        //echo "<div class='cell'><a href='javascript:deleteSaleData(`".$row[Salecode]."`);'>Delete</a></div>";
+        //echo "</div>";
+      
+
         ?>
         <form name="myform" action="<?php echo $_SERVER['$PHP_SELF']; ?>" method="POST">
-          <input type="hidden" name="saledate" id="saledate" value="<?php echo $row['Saledate']; ?>" />
+          <input type="hidden" name="salecode" id="salecode" value="<?php echo $row['Salecode']; ?>" />
           <button type="submit" href="javascript:deleteSaleData();">Delete</button>
         </form>
-        </div>
-      <?php } ?>
-    </div>
+        <!--           <div class='cell'><a href='javascript:deleteSaleData(`".$row[Salecode]."`);'>Delete</a></div> -->
+      </div>
+    <?php } ?>
   </div>
+</div>
 </div>
 <br>
 <script>
   document.getElementById('breed').value = "<?php echo $breed_param; ?>";
 </script>
+
 <script>
   function sortTable(column) {
     var sortOrder = '<?php echo $sortOrder === 'ASC' ? 'DESC' : 'ASC'; ?>';
@@ -114,7 +141,10 @@ if (!empty($_POST)) {
 <script>
   function getValues() {
     var breed = document.getElementById('breed').value;
+
     var link = "manage_data.php?breed=" + breed;
+    //alert(link);
+
     window.open(link, "_self");
     if (breed == "") {
       alert("Please Select Breed Category ");
@@ -125,15 +155,17 @@ if (!empty($_POST)) {
 <script>
   <?php if (!empty($_POST)) { ?>
     var breed = document.getElementById('breed').value;
-    var saledate = document.getElementById('saledate').value;
+    //alert(bred);
+    //alert('<?php echo $salecode; ?>');
     var result = "";
-    if (confirm("Are you sure, you want to delete records with sale date -" + saledate + "?")) {
+    if (confirm("Are you sure, you want to delete -" + '<?php echo $salecode; ?>' + "?")) {
       txt = "You pressed OK!";
       result = "<?php echo deleteSalecode($breed_param, $salecode); ?>";
     } else {
       result = "You pressed Cancel!";
     } alert(result);
     getValues();
+    //alert("Are You Sure?");
     <?php
   }
   ?>
