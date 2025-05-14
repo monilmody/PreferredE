c6abf22574e01e28aa0e39c3d2b0653687b3c6f4
monil mody <you@example.com>
2025-05-14 14:03:50 -0400

    securing the password

--- db-settings.php
+++ db-settings.php
@@ -1,19 +1,27 @@
 <?php
 
+require 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Load .env
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 //Development Database Information
-	$db_host = "preferredequinesalesresultsdatabase.cdq66kiey6co.us-east-1.rds.amazonaws.com"; //Host address (most likely localhost)
-	$db_name = "horse"; //Name of Database
-	$db_user = "preferredequine"; //Name of database user
-	$db_pass = "914MoniMaker77$$"; //Password for database user
-	$db_table_prefix = ""; // if the table prefix exists use this variable as a global
-	
+$db_host = $_ENV['DB_HOST']; //Host name of rds database
+$db_name = $_ENV['DB_NAME']; //Name of Database
+$db_user = $_ENV['DB_USER']; //Name of database user
+$db_pass = $_ENV['DB_PASS']; //Password for database user
+$db_table_prefix = ""; // if the table prefix exists use this variable as a global
+
 
 //following variable declaration
-	GLOBAL $errors;
-	GLOBAL $successes;
+global $errors;
+global $successes;
 
-	$errors = array();
-	$successes = array();
+$errors = array();
+$successes = array();
 
 // 1. Create a database connection
 //$connection = mysql_connect(DB_HOST,DB_USER,DB_PSWD);
@@ -24,15 +32,13 @@
 
 /* Create a new mysqli object with database connection parameters */
 
-	$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-	GLOBAL $mysqli;
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+global $mysqli;
 
-	if(mysqli_connect_errno()) {
-  //display the reason for mysql connection error.
-	 echo "Connection Failed1: " . mysqli_connect_errno();
-    exit();
-	}else{
-     //echo "Connection Successful";
-	}
-?>
-
+if (mysqli_connect_errno()) {
+	//display the reason for mysql connection error.
+	echo "Connection Failed1: " . mysqli_connect_errno();
+	exit();
+} else {
+	//echo "Connection Successful";
+}
