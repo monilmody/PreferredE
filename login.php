aeff112a14a37b7ecec5679ead29f28a8cb51bd0
monil mody <you@example.com>
2025-05-21 10:39:32 -0400

    adding the functionality so that the session page returns to the current page not index

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ echo '<br>';
 <!--   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
 <!--   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
 <!--   <link rel="stylesheet" href="assets/css/table.css"> -->
-  
+
 <!--   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css"> -->
 <!--   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script> -->
 
@@ -29,63 +29,63 @@ require_once("config.php");
 // }
 
 //Forms posted
-if(!empty($_POST)) {
+if (!empty($_POST)) {
 	$errors = array();
 	$username = trim($_POST["username"]);
 	$password = trim($_POST["password"]);
-	
+
 	//Perform some validation
-	if($username == "") {
+	if ($username == "") {
 		$errors[] = "Enter Username";
 	}
-	if($password == "") {
+	if ($password == "") {
 		$errors[] = "Enter Password";
 	}
-	if(count($errors) == 0) {
+	if (count($errors) == 0) {
 		//retrieve the records of the user who is trying to login
 		$userdetails = fetchUserDetails($username);
 		if ($userdetails["USERNAME"] == "") {
-		    $errors[] = "Account Not Found";
+			$errors[] = "Account Not Found";
 		}
 		//See if the user"s account is activated
 		//$errors[]= $userdetails["Active"];
-		if($userdetails["ACTIVE"] == "N") {
+		if ($userdetails["ACTIVE"] == "N") {
 			$errors[] = "Account Not Authorized";
 		} else {
 			//Hash the password and use the salt from the database to compare the password.
 			//$entered_pass = generateHash($password, $userdetails["Password"]);
-			
-		    if($password != $userdetails["PASSWORD"]) {
+
+			if ($password != $userdetails["PASSWORD"]) {
 				$errors[] = "Invalid Password";
 			} else {
-			    //header("Location:index.php");
+				//header("Location:index.php");
 				//Passwords match! we"re good to go"
-// 				$loggedInUser = new loggedInuser();
-// 				$loggedInUser->email = $userdetails["EMAIL"];
-// 				$loggedInUser->user_id = $userdetails["USER_ID"];
-// 				$loggedInUser->password = $userdetails["PASSWORD"];
-// 				$loggedInUser->first_name = $userdetails["FNAME"];
-// 				$loggedInUser->last_name = $userdetails["LNAME"];
-// 				$loggedInUser->username = $userdetails["USERNAME"];
-				
+				// 				$loggedInUser = new loggedInuser();
+				// 				$loggedInUser->email = $userdetails["EMAIL"];
+				// 				$loggedInUser->user_id = $userdetails["USER_ID"];
+				// 				$loggedInUser->password = $userdetails["PASSWORD"];
+				// 				$loggedInUser->first_name = $userdetails["FNAME"];
+				// 				$loggedInUser->last_name = $userdetails["LNAME"];
+				// 				$loggedInUser->username = $userdetails["USERNAME"];
+
 				//pass the values of $loggedInUser into the session -
 				// you can directly pass the values into the array as well.
-				
+
 				//$_SESSION["ThisUser"] = $loggedInUser;
-			    //echo  $userdetails["USERNAME"];
-			    $_SESSION["UserActive"] = $userdetails["ACTIVE"];
+				//echo  $userdetails["USERNAME"];
+				$_SESSION["UserActive"] = $userdetails["ACTIVE"];
 				$_SESSION["UserName"] = $userdetails["USERNAME"];
 				$_SESSION["UserRole"] = $userdetails["USERROLE"];
-				
+
 				//echo $_SESSION["UserName"];
 
 				setcookie("LoggedInUser", $userdetails["USERNAME"], time() + 3600, "/");
 
 				//now that a session for this user is created
 				//Redirect to this users account page
-				header("Location:index.php?user=".$userdetails["USERNAME"],true);
-				//echo "<script>window.top.location=http://preferredequineresults.com/PreferredE/index.php'</script>";
-				die();
+				$redirect_url = isset($_GET['redirect']) ? urldecode($_GET['redirect']) : "index.php";
+				header("Location: $redirect_url");
+				exit();
 				//exit();
 			}
 		}
@@ -96,33 +96,35 @@ ob_end_flush();
 
 
 <style type="text/css" media="screen">
-    @import url("style/css/style.css");
+	@import url("style/css/style.css");
 </style>
+
 <body>
-<div class="main-block">
-    <blockquote>
-        <?php print_r($errors[0]); ?>
-    </blockquote>
-    <form name="login" action="<?php $_SERVER["PHP_SELF"]; ?>" method="post" >
-        <hr>
-        <hr>
-        <div style="text-align:center;border:black">
-        <input type="text" name="username" id="username" placeholder="user name"  required/>
-		</div>
-		<div style="text-align:center;">
-		<input type="password" name="password" id="password" placeholder="Password" required/>
-        </div>
-        <hr>
+	<div class="main-block">
+		<blockquote>
+			<?php print_r($errors[0]); ?>
+		</blockquote>
+		<form name="login" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?><?php echo isset($_GET['redirect']) ? '?redirect=' . urlencode($_GET['redirect']) : ''; ?>" method="post">
+			<hr>
+			<hr>
+			<div style="text-align:center;border:black">
+				<input type="text" name="username" id="username" placeholder="user name" required />
+			</div>
+			<div style="text-align:center;">
+				<input type="password" name="password" id="password" placeholder="Password" required />
+			</div>
+			<hr>
 
-        <div class="btn-block" style="text-align:center;">
-            <button type="submit" href="/">Submit</button>
+			<?php if (isset($_GET['redirect'])): ?>
+				<input type="hidden" name="redirect" value="<?php echo htmlspecialchars($_GET['redirect']); ?>">
+			<?php endif; ?>
+			<div class="btn-block" style="text-align:center;">
+				<button type="submit" href="/">Submit</button>
 
-        </div>
-    </form>
-</div>
+			</div>
+		</form>
+	</div>
 
-<script>
-        document.getElementById("username").focus();    
-</script>
-
-
+	<script>
+		document.getElementById("username").focus();
+	</script>
--- session_page.php
+++ session_page.php
@@ -1,19 +1,21 @@
 <?php
-
 // Set session timeout duration (in seconds)
 $timeout_duration = 1800; // 30 minutes
 
+// Get the current page URL
+$current_url = urlencode($_SERVER['REQUEST_URI']); // Encode it for use in URL
+
 // Check if the user is logged in
 if (!isset($_SESSION["UserName"])) {
-    header("Location: login.php");
+    header("Location: login.php?redirect=$current_url");
     exit();
 }
 
 // Session timeout logic
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
-    session_unset();     // Unset $_SESSION
-    session_destroy();   // Destroy session data
-    header("Location: login.php?timeout=true");
+    session_unset();
+    session_destroy();
+    header("Location: login.php?timeout=true&redirect=$current_url");
     exit();
 }
 $_SESSION['LAST_ACTIVITY'] = time(); // Update last activity timestamp
